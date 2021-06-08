47ac30be6995b001a2f9ebbe1e5e130d585caecb
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-08 17:45:16 -0300

    Colocando o Projeto na arquitetura MVC

---
+++ application/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, request, url_for
+import os
+
+template_folder = os.path.abspath('application/view/templates')
+static_folder = os.path.abspath("application/view/static")
+
+app = Flask(__name__, template_folder = template_folder, static_folder = static_folder)
+
+'''from application.controller import home_controller'''
