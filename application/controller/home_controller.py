e95352f8e57cd6f55ee1ed49abec22b16b840d92
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-13 14:45:11 -0300

    Adicionando o rota de inserir ferramentas

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,6 +1,7 @@
 from application import app
 from application.model.dao.ferramentasDAO import FerramentasDAO
 from flask import render_template, request, url_for, redirect
+from application.model.entity.ferramentas import Ferramentas
 
 ferramentaDAO = FerramentasDAO()
 ferramenta_list = ferramentaDAO.lista_ferramentas()
@@ -9,4 +10,15 @@ ferramenta_list = ferramentaDAO.lista_ferramentas()
 
 @app.route("/", methods=['GET'])
 def home():
+    return render_template("index.html", ferramenta_list = ferramenta_list)
+
+@app.route("/inserir", methods=['POST'])
+def inserir():
+    titulo = request.form.get('titulo', None)
+    link = request.form.get('link', None)
+    descricao = request.form.get('descricao', None)
+    tags = request.form.get('tags', None).split(" ")
+    id = len(ferramenta_list) + 1
+    ferramentas = Ferramentas(id, titulo, link, descricao, tags)
+    ferramentas.append(ferramentas)
     return render_template("index.html", ferramenta_list = ferramenta_list)
