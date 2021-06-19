8891fa834f5ba27efc1a5e16dc0d828299b8c137
Daniel Cabral <danielccabral1981@gmail.com>
2021-06-19 13:59:46 -0300

    adicionando mai um card

--- application/model/dao/ferramentasDAO.py
+++ application/model/dao/ferramentasDAO.py
@@ -4,8 +4,9 @@ from application.model.entity.ferramentas import Ferramentas
 ferramentas_list = [
     Ferramentas(1, "Django", "https://www.djangoproject.com/", "framework para projeto em python WEB", ["python", "framework"]),
     Ferramentas(2, "React", "https://pt-br.reactjs.org/", "framework para projetos em JS", ["JS", "framework"]),
-
+    Ferramentas(3,"Trabalho", "https://vassouras.myopenlms.net/mod/assign/view.php?id=39348", "Entregar trabalho de LabHIPER", ["trabalho", "faculdade"] )
 ]
+
 
 class FerramentasDAO():
     def __init__(self):
